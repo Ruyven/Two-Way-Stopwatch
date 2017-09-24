09baa9285197588b9393be82abd3b4e151748a84
Alex Decker <13271370+Ruyven@users.noreply.github.com>
2017-09-24 21:34:08 +0900

    Initial Commit

---
+++ Stopwatch/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Stopwatch
+//
+//  Created by Alex Decker on 2017-09-24.
+//  Copyright © 2017 me. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
