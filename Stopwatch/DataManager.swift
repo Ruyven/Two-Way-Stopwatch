bbc535a50d43bf35f51d5247ce7d6c2583ffa463
Alex Decker <13271370+Ruyven@users.noreply.github.com>
2017-09-28 12:16:43 +0900

    - show time (h:mm:ss.s) - save device with base time and sessions in local database

---
+++ Stopwatch/DataManager.swift
@@ -0,0 +1,209 @@
+//
+//  DataManager.swift
+//  Stopwatch
+//
+//  Created by Alex Decker on 2017-09-26.
+//  Copyright © 2017 me. All rights reserved.
+//
+
+import Cocoa
+import IOKit
+
+import SQLite
+
+class DataManager {
+    static var manager = DataManager()
+    
+    struct Constants {
+        static let dbFilename = "db.sqlite3"
+    }
+    
+    private var dbConnection: Connection?
+    
+    private func openDBConnection() -> Connection? {
+        if let dbConnection = self.dbConnection {
+            return dbConnection
+        } else {
+            let path = NSSearchPathForDirectoriesInDomains(
+                .documentDirectory, .userDomainMask, true
+                ).first!
+            let fileManager = FileManager.default
+            
+            // create directory iff it doesn’t exist
+            do {
+                try fileManager.createDirectory(
+                    atPath: path, withIntermediateDirectories: true, attributes: nil
+                )
+            } catch {
+                //
+            }
+            
+            let filepath = "\(path)/\(Constants.dbFilename)"
+            
+            // check if file exists; if not, create it
+            if !fileManager.fileExists(atPath: filepath) {
+                guard let filepathInBundle = Bundle.main.resourceURL?.appendingPathComponent(Constants.dbFilename).path else {
+                    return nil
+                }
+                do {
+                    try fileManager.copyItem(atPath: filepathInBundle, toPath: filepath)
+                } catch {
+                    print("error: \(error)")
+                    return nil
+                }
+            }
+            
+            let db = try? Connection("\(path)/\(Constants.dbFilename)")
+            self.dbConnection = db
+            return db
+        }
+    }
+    
+    var uuid: String
+    
+    init() {
+        /*if let db = self.openDBConnection() {
+            
+        }*/
+        
+        // get this device's serial number
+        // Get the platform expert
+        let platformExpert: io_service_t = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("IOPlatformExpertDevice"));
+        
+        // Get the serial number as a CFString ( actually as Unmanaged<AnyObject>! )
+        let serialNumberAsCFString = IORegistryEntryCreateCFProperty(platformExpert, kIOPlatformSerialNumberKey as CFString, kCFAllocatorDefault, 0);
+        
+        // Release the platform expert (we're responsible)
+        IOObjectRelease(platformExpert);
+        
+        // Take the unretained value of the unmanaged-any-object
+        // (so we're not responsible for releasing it)
+        // and pass it back as a String or, if it fails, an empty string
+        self.uuid = serialNumberAsCFString!.takeUnretainedValue() as! String
+    }
+    
+    func getTotalTime(completion: ((Double?) -> Void)) {
+        guard let db = self.openDBConnection() else {
+            completion(nil)
+            return
+        }
+        
+        // time in hours
+        var totalTime: Double = 0
+        
+        // first, add up current times of all devices
+        let devices = Table("devices")
+        guard let dquery = try? db.prepare(devices) else {
+            completion(nil)
+            return
+        }
+        
+        var selfExists = false
+        for device in dquery {
+            totalTime += device[.baseHours]
+            if device[.uuid] == self.uuid {
+                selfExists = true
+            }
+        }
+        
+        if !selfExists {
+            self.insertDeviceIntoDatabase()
+        }
+        
+        // now go through the sessions
+        let sessionsTable = Table("sessions")
+        _=sessionsTable.order(Expression<Double>.startTime.asc)
+        guard let squery = try? db.prepare(sessionsTable) else {
+            completion(totalTime)
+            return
+        }
+        
+        var sessions: [Session] = []
+        
+        for session in squery {
+            sessions.append(Session(startTime: session[.startTime], hours: session[.hours]))
+        }
+        
+        // filter / merge sessions
+        var filteredSessions = [Session]()
+        let forwardSessions = sessions.filter({$0.direction > 0})
+        let backwardSessions = sessions.filter({$0.direction < 0})
+        for sessions in [forwardSessions, backwardSessions] {
+            for (i, session) in sessions.enumerated() {
+                var adjSession = session
+                if i < sessions.count - 1 {
+                    let nextSession = sessions[i+1]
+                    if nextSession.startTime < adjSession.endTime {
+                        adjSession.endTime = nextSession.startTime
+                    }
+                }
+                filteredSessions.append(adjSession)
+            }
+        }
+        
+        for session in filteredSessions {
+            totalTime += session.hours
+        }
+        
+        completion(totalTime)
+    }
+    
+    private func insertDeviceIntoDatabase() {
+        guard let db = self.openDBConnection() else {
+            return
+        }
+        
+        let devices = Table("devices")
+        do {
+            try db.run(devices.insert(.uuid <- self.uuid, .baseHours <- 0))
+        } catch {
+            print("error: \(error)")
+        }
+    }
+    
+    func logSession(startTime: Date, hours: Double) {
+        guard let db = self.openDBConnection() else {
+            return
+        }
+        
+        let sessions = Table("sessions")
+        do {
+            try db.run(sessions.insert(.device_uuid <- self.uuid, .startTime <- startTime, .hours <- hours))    // note that hours might be negative. The session duration is abs(hours).
+        } catch {
+            print("logSession: \(error)")
+        }
+    }
+}
+
+extension Expression {
+    static var uuid: Expression<String> { return Expression<String>("uuid") }
+    static var baseHours: Expression<Double> { return Expression<Double>("baseHours") }
+    
+    static var device_uuid: Expression<String> { return Expression<String>("device_uuid") }
+    static var startTime: Expression<Date> { return Expression<Date>("startTime") }
+    static var hours: Expression<Double> { return Expression<Double>("hours") }
+}
+
+struct Session {
+    var startTime: Date
+    var hours: Double
+    
+    var direction: Double {
+        if hours >= 0 {
+            return 1
+        } else {
+            return -1
+        }
+    }
+    var endTime: Date {
+        get {
+            let seconds = abs(hours) * 3600
+            return startTime.addingTimeInterval(seconds)
+        }
+        set {
+            let seconds = newValue.timeIntervalSince(self.startTime)
+            self.hours = seconds/3600 * self.direction
+        }
+    }
+    
+}
--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -10,10 +10,24 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var valueLabel: NSTextField! {
+        didSet {
+            valueLabel.font = NSFont.monospacedDigitSystemFont(ofSize: 40, weight: NSFont.Weight.regular)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        DataManager.manager.getTotalTime { value in
+            guard let value = value else {
+                return
+            }
+            
+            self.value = value
+            self.updateDisplay()
+        }
     }
 
     override var representedObject: Any? {
@@ -21,7 +35,78 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    var value: Double = 0 // hours
+    var displayTimer: Timer?
+    var startTime: Date = Date()
+    var direction: Double = 0
+    
+    var isRunning: Bool {
+        return self.direction != 0
+    }
 
+    @IBAction func startForward(_ sender: Any) {
+        if self.isRunning {
+            self.pause()
+        }
+        self.startTime = Date()
+        self.direction = 1
+        self.displayTimer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(updateDisplay), userInfo: nil, repeats: true)
+    }
+    @IBAction func startBackward(_ sender: Any) {
+        if self.isRunning {
+            self.pause()
+        }
+        self.startTime = Date()
+        self.direction = -1
+        self.displayTimer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(updateDisplay), userInfo: nil, repeats: true)
+    }
+    
+    // seconds
+    var currentTime: Double {
+        return Date().timeIntervalSince(self.startTime) * self.direction
+    }
+    
+    var valuePlusCurrent: Double {
+        return self.value + self.currentTime / 3600     // hours
+    }
+    
+    @IBAction func pause(_ sender: Any? = nil) {
+        guard self.isRunning else {
+            return
+        }
+        
+        DataManager.manager.logSession(startTime: self.startTime, hours: self.currentTime/3600)
+        
+        self.value = self.valuePlusCurrent
 
+        self.displayTimer?.invalidate()
+        self.direction = 0
+    }
+    
+    @objc func updateDisplay() {
+        var time = self.valuePlusCurrent
+        
+        let vorzeichen: String
+        if time >= 0 {
+            vorzeichen = ""
+        } else {
+            vorzeichen = "-"
+            time = 0 - time
+        }
+        
+        let hours = Int(time)
+        time = time - Double(hours)
+        time *= 60
+        let minutes = Int(time)
+        time = time - Double(minutes)
+        time *= 60
+        let seconds = Int(time)
+        time -= Double(seconds)
+        let tenths = Int(time*10)
+        
+        self.valueLabel.stringValue = String(format: "\(vorzeichen)%d:%02d:%02d.%d", hours, minutes, seconds, tenths)
+    }
+    
 }
 
